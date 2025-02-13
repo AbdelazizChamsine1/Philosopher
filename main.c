bae32b07cc0363a03a64956e3043b5c95dd88dc0
Abdelaziz Chamsine <achamsin@c2r4s4.42beirut.com>
2025-02-13 13:11:51 +0200

    first commit

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
-#include "stdio.h"
-
-int main()
+int main(int argc, char **argv)
 {
-    printf("hello");
-    return 0;
+    t_data data;
+
+    if (argc != 5 && argc != 6)
+        return (printf("Invalid arguments\n"), 1);
+    if (init_data(&data, argc, argv) != 0)
+        return (printf("Initialization failed\n"), 1);
+    if (create_threads(&data) != 0)
+        return (printf("Thread creation failed\n"), 1);
+    join_threads(&data);
+    cleanup(&data);
+    return (0);
 }
